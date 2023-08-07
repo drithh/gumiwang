8e2a0d7ded45a62cf282b35f96de725651326a47
drithh <adrielalfeus@gmail.com>
2023-08-08 06:33:13 +0700

    remove carousel

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,12 +1,8 @@
 import Image from 'next/image';
 import { getHalamanUtama } from '@/src/sanity/sanity-utils';
-import Carousel from '~/components/carousel';
+// import Carousel from '~/components/carousel';
 
 export default async function Home() {
   const halamanUtama = await getHalamanUtama();
-  return (
-    <div>
-      <Carousel slides={halamanUtama.slides} />
-    </div>
-  );
+  return <div>{/* <Carousel slides={halamanUtama.slides} /> */}</div>;
 }
