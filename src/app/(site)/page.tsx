4cebefb911aaace41a2a92152c9dc25d291ce90c
drithh <adrielalfeus@gmail.com>
2023-08-08 06:39:44 +0700

    add card file

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image';
 import { getHalamanUtama } from '@/src/sanity/sanity-utils';
-import Carousel from '../../components/carousel';
+// import Carousel from '~/components/carousel';
+import Card from '~/components/card';
 
 export default async function Home() {
   const halamanUtama = await getHalamanUtama();
   return (
     <div>
-      <Carousel slides={halamanUtama.slides} />
+      <Card slides={halamanUtama.slides} />
+      {/* <Carousel slides={halamanUtama.slides} /> */}
     </div>
   );
 }
