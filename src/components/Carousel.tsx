50a9383276e615298b40d75d3e69e873d1f83aba
drithh <adrielalfeus@gmail.com>
2023-08-08 05:55:42 +0700

    add carousel button and autoplay

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,15 +1,59 @@
 'use client';
 
-import useEmblaCarousel from 'embla-carousel-react';
+import useEmblaCarousel, {
+  EmblaCarouselType,
+  EmblaOptionsType,
+} from 'embla-carousel-react';
+import Autoplay from 'embla-carousel-autoplay';
 import Image from 'next/image';
 import { Slide } from '~/types';
+import { NextButton, PrevButton } from './carousel-button-arrow';
+import { useCallback, useEffect, useState } from 'react';
 
 interface CarouselProps {
   slides: Slide[];
 }
 
 function Carousel({ slides }: CarouselProps) {
-  const [emblaRef] = useEmblaCarousel();
+  const options = { loop: true } as EmblaOptionsType;
+  const [emblaRef, emblaApi] = useEmblaCarousel(options, [Autoplay()]);
+  const [prevBtnDisabled, setPrevBtnDisabled] = useState(true);
+  const [nextBtnDisabled, setNextBtnDisabled] = useState(true);
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [scrollSnaps, setScrollSnaps] = useState<number[]>([]);
+
+  const scrollPrev = useCallback(
+    () => emblaApi && emblaApi.scrollPrev(),
+    [emblaApi]
+  );
+  const scrollNext = useCallback(
+    () => emblaApi && emblaApi.scrollNext(),
+    [emblaApi]
+  );
+  const scrollTo = useCallback(
+    (index: number) => emblaApi && emblaApi.scrollTo(index),
+    [emblaApi]
+  );
+
+  const onInit = useCallback((emblaApi: EmblaCarouselType) => {
+    setScrollSnaps(emblaApi.scrollSnapList());
+  }, []);
+
+  const onSelect = useCallback((emblaApi: EmblaCarouselType) => {
+    setSelectedIndex(emblaApi.selectedScrollSnap());
+    setPrevBtnDisabled(!emblaApi.canScrollPrev());
+    setNextBtnDisabled(!emblaApi.canScrollNext());
+  }, []);
+
+  useEffect(() => {
+    if (!emblaApi) return;
+
+    onInit(emblaApi);
+    onSelect(emblaApi);
+    emblaApi.on('reInit', onInit);
+    emblaApi.on('reInit', onSelect);
+    emblaApi.on('select', onSelect);
+  }, [emblaApi, onInit, onSelect]);
   return (
     <div className="overflow-hidden" ref={emblaRef}>
       <div className="flex h-screen">
@@ -28,6 +72,10 @@ function Carousel({ slides }: CarouselProps) {
             <Image src={slide.gambar} alt={slide.alt} fill />
           </div>
         ))}
+      </div>
+      <div className="absolute bottom-0 left-0 w-full flex justify-between items-center h-full">
+        <PrevButton onClick={scrollPrev} disabled={prevBtnDisabled} />
+        <NextButton onClick={scrollNext} disabled={nextBtnDisabled} />
       </div>
     </div>
   );
