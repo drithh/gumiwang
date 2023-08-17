6b1cfc106794d7cd0cdb4e352a9521f2700ea01b
drithh <adrielalfeus@gmail.com>
2023-08-17 16:40:14 +0700

    add pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,23 @@
-import './src/env.mjs';
+import "./src/env.mjs";
+
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+});
 
 /** @type {import("next").NextConfig} */
 const config = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'cdn.sanity.io',
-        port: '',
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        port: "",
       },
     ],
   },
   /** ... */
 };
 
-export default config;
+export default withPWA(config);
